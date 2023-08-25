8d4fdd0c16a85f9c854ddbb5068cee2dee1c7d64
Don Di3go <dondi3go@users.noreply.github.com>
2023-08-25 20:26:22 +0200

    Update Superklub + update HttpClient to match new IHttpClient interface

--- Assets/Scripts/SuperklubForUnity/HttpClient.cs
+++ Assets/Scripts/SuperklubForUnity/HttpClient.cs
@@ -15,26 +15,64 @@ public class HttpClient : IHttpClient
     /// <summary>
     /// 
     /// </summary>
-    public async Task<HttpResponse> PostAsync(string url, string jsonString)
+    public async Task<HttpResponse> PostAsync(
+        string url, 
+        string jsonString,
+        List<CustomRequestHeader>? customRequestHeaders = null,
+        CustomResponseHeader? customResponseHeader = null)
     {
         UnityWebRequest req = WebRequest.Post(url, jsonString);
+        AddRequestHeaders(req, customRequestHeaders);
         if (!await WebRequest.FullProcess(req, verbose))
         {
             return new HttpResponse((int)req.responseCode, req.error);
         }
-        return new HttpResponse((int)req.responseCode, req.downloadHandler.text);
+        string customHeaderValue = GetResponseHeaderValue(req, customResponseHeader);
+        return new HttpResponse((int)req.responseCode, req.downloadHandler.text, customHeaderValue);
     }
 
     /// <summary>
     /// 
     /// </summary>
-    public async Task<HttpResponse> GetAsync(string url)
+    public async Task<HttpResponse> GetAsync(
+        string url,
+        List<CustomRequestHeader>? customRequestHeaders = null,
+        CustomResponseHeader? customResponseHeader = null)
     {
         UnityWebRequest req = WebRequest.Get(url);
+        AddRequestHeaders(req, customRequestHeaders);
         if (!await WebRequest.FullProcess(req, verbose))
         {
             return new HttpResponse((int)req.responseCode, req.error);
         }
-        return new HttpResponse((int)req.responseCode, req.downloadHandler.text);
+        string customHeaderValue = GetResponseHeaderValue(req, customResponseHeader);
+        return new HttpResponse((int)req.responseCode, req.downloadHandler.text, customHeaderValue);
+    }
+
+    /// <summary>
+    /// Add header (key+value) to request
+    /// </summary>
+    private void AddRequestHeaders(UnityWebRequest req, List<CustomRequestHeader>? customRequestHeaders)
+    {
+        if(customRequestHeaders == null)
+        {
+            return;
+        }
+        foreach(CustomRequestHeader header in customRequestHeaders)
+        {
+            req.SetRequestHeader(header.Key, header.Value);
+        }
+    }
+
+    /// <summary>
+    /// Extract header value from response
+    /// </summary>
+    private string GetResponseHeaderValue(UnityWebRequest req, CustomResponseHeader? customResponseHeader)
+    {
+        if(customResponseHeader == null)
+        {
+            return string.Empty;
+        }
+        return req.GetResponseHeader(customResponseHeader.Key);
     }
 }
